2b1e3e474e59842b246f2e460b32006e6f4e0dc4
AdrianoR85 <adrianorosa085@gmail.com>
2025-08-04 15:35:26 -0300

    added a image in the Login Page

--- Employee_Manager_System/app.py
+++ Employee_Manager_System/app.py
@@ -1,6 +1,11 @@
 from customtkinter import *
+from PIL import Image
 
 root = CTk()
 root.geometry('930x478')
 root.resizable(0,0)
+root.title('Login Page')
+image = CTkImage(Image.open("assets/cover.png"), size=(930,478))
+imageLabel=CTkLabel(root, image=image, text="")
+imageLabel.place(x=0, y=0)
 root.mainloop()
